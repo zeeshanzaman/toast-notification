a0b009e68a736ec2a7547e94a3b7a78a74d3fd0d
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-10-12 20:48:30 +0500

    create an array of message

--- custom.js
+++ custom.js
@@ -1,2 +1,9 @@
 const button = document.getElementById('button')
 const toast = document.getElementById('toasts')
+
+const message = [
+    'Message One',
+    'Messge Two',
+    'Message Three',
+    'Message Four'
+]
